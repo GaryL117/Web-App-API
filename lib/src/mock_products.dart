b2c5da9f7d2620c3674cfb21cf105b6b5be3f5d2
GaryL117 <garyl117agp@gmail.com>
2019-06-26 13:10:00 -0400

    Created to display mock products

---
+++ lib/src/mock_products.dart
@@ -0,0 +1,15 @@
+import 'product.dart';
+
+final mockProducts = <Product>[
+  Product(11, 'Soft 2000'),
+  Product(12, 'Auburn Oak'),
+  Product(13, 'Walnut Oak'),
+  Product(14, 'Exotic Oak'),
+  Product(15, 'Auburn Ash'),
+  Product(16, 'Walnut Ash'),
+  Product(17, 'Exotic Ash'),
+  Product(18, 'Auburn Maple'),
+  Product(19, 'Walnut Maple'),
+  Product(20, 'Golden Maple')
+];
+
