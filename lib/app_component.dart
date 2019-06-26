4e7af576f8b20fe5f128a8f4c3381042ca095d5e
GaryL117 <garyl117agp@gmail.com>
2019-06-26 14:02:16 -0400

    Replaced product = product(id, name) with List of mock products in AppComponent

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,12 +1,14 @@
 import 'package:angular/angular.dart';
+import 'package:angular_app/src/mock_products.dart';
 import 'package:angular_app/src/product.dart';
+
 @Component(
   selector: 'my-app',
-  templateUrl: 'app_component.html'
-
+  templateUrl: 'app_component.html',
+ directives: [coreDirectives],
 
 )
 class AppComponent {
  final title = 'RenoRun Products and Orders';
- Product product = Product(1, 'Chair 9000+');
+ List<Product> products = mockProducts;
 }
