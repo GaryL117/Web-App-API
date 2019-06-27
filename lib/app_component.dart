281f7253db2d0626d052932f4dc7a706d81bbdda
GaryL117 <garyl117agp@gmail.com>
2019-06-27 15:19:10 -0400

    Used ngOnInit lifecycle hook to get product data when AppComponent activates. Defined ProductService as provider for AppComponent. Designed service to return a Future and the component to get the data from the Future

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,18 +1,32 @@
 import 'package:angular/angular.dart';
 import 'package:angular_app/src/mock_products.dart';
 import 'package:angular_app/src/product.dart';
-
+import 'src/product_component.dart';
+import 'src/product_service.dart';
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
-  directives: [coreDirectives],
+  directives: [coreDirectives, ProductComponent],
   styleUrls: ['app_component.css'],
-
+  providers: [ClassProvider(ProductService)],
 )
-class AppComponent {
+class AppComponent implements OnInit{
  final title = 'RenoRun Products and Orders';
- List<Product> products = mockProducts;
+ final ProductService _productService;
+ List<Product> products;
  Product selected;
  void onSelect(Product product) => selected = product;
+
+
+ AppComponent(this._productService);
+
+ Future<void> _getProducts() async {
+   products = (await _productService.getAll());
+
+ }
+
+ void ngOnInit() => _getProducts();
 }
+
+
 
