21e8e121348261ec2e400c7552a94df566c53af7
GaryL117 <garyl117agp@gmail.com>
2019-06-28 15:38:35 -0400

    Created file and added navigation around app

---
+++ lib/src/dashboard_component.dart
@@ -0,0 +1,27 @@
+import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+
+import 'product.dart';
+import 'product_service.dart';
+import 'route_paths.dart';
+
+@Component (
+  selector: 'my-dashboard',
+  templateUrl: 'dashboard_component.html',
+  directives: [coreDirectives, routerDirectives],
+)
+class DashboardComponent implements OnInit {
+  List<Product> products;
+  final ProductService _productService;
+
+  String productUrl(int id) => RoutePaths.product.toUrl(parameters: {idParam: '$id'});
+
+  DashboardComponent(this._productService);
+
+
+  @override
+  void ngOnInit() async {
+    products = (await _productService.getAll()).skip(1).take(4).toList();
+  }
+
+}
