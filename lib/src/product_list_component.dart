12a9d0a11eb44b9f1e55c6d337d40007c39d94de
GaryL117 <garyl117agp@gmail.com>
2019-07-30 09:50:59 -0400

    Replaced mock product list with list from api

--- lib/src/product_list_component.dart
+++ lib/src/product_list_component.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
+import 'package:http/http.dart';
 
 import 'route_paths.dart';
 import 'product.dart';
@@ -14,33 +17,52 @@ import 'product_service.dart';
   styleUrls: ['product_list_component.css'],
   pipes: [commonPipes],
 )
-class ProductListComponent implements OnInit{
+class ProductListComponent implements OnInit {
   final title = 'Products: ';
-  final Router  _router;
+  final Router _router;
   final ProductService _productService;
   List<Product> products;
   Product selected;
+  dynamic data;
+  dynamic Data_Org;
+  dynamic Data_Org2;
+
+
   void onSelect(Product product) => selected = product;
 
 
- ProductListComponent(this._productService, this._router);
+  ProductListComponent(this._productService, this._router,);
 
- Future<void> _getProducts() async {
-   products = (await _productService.getAll());
+  Future<void> _getProducts() async {
+    Response _products = await http.get('http://127.0.0.1:5000/products');
+    data = jsonDecode(_products.body);
+    Data_Org = Product(data[0]['id'] as int, data[0]['name'], data[0]['description'], (data[0]['price']) as double);
+    Data_Org2 = Product(data[1]['id'] as int, data[1]['name'], data[1]['description'], (data[1]['price']) as double);
 
- }
+//    List<Product> data = [
+//      Product(
+//          int.parse('id'),
+//          'name',
+//          'description',
+//          double.parse('price'))
+//    ];
+    print(data);
+    print(data.runtimeType);
 
 
 
- void ngOnInit() => _getProducts();
 
-  String _productUrl(int id) =>
-      RoutePaths.product.toUrl(parameters: {idParam: '$id'});
 
- Future<NavigationResult> gotoDetail() =>
-     _router.navigate(_productUrl(selected.id));
+//    products = (await _productService.getAll());
+  }
+    void ngOnInit() => _getProducts();
 
-}
 
+  }
+
+  @override
+  void ngOnInit() {
+
+  }
 
 
