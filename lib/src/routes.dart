55102c4adfe799f5b48663ecec78bba22613a51c
GaryL117 <garyl117agp@gmail.com>
2019-07-30 09:48:20 -0400

    added order list route

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -2,7 +2,7 @@ import 'package:angular_router/angular_router.dart';
 import 'route_paths.dart';
 import 'product_list_component.template.dart' as product_list_template;
 import 'dashboard_component.template.dart' as dashboard_template;
-import 'product_component.template.dart' as product_template;
+import 'order_list_component.template.dart' as order_list_template;
 
 export 'route_paths.dart';
 
@@ -18,14 +18,21 @@ class Routes {
     useAsDefault: true,
   );
 
-  static final product = RouteDefinition(
-    routePath: RoutePaths.product,
-    component: product_template.ProductComponentNgFactory,
+  static final order = RouteDefinition(
+  routePath: RoutePaths.order,
+  component: order_list_template.OrderListComponentNgFactory,
   );
+
+
+ // static final product = RouteDefinition(
+  //  routePath: RoutePaths.product,
+  //  component: product_template.ProductComponentNgFactory,
+  //);
 
   static final all = <RouteDefinition>[
     products,
     dashboard,
-    product,
+    order
+    //product,
   ];
 }
